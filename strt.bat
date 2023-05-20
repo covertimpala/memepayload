c8c98a298a1a8e89f877bac88035832316a1564c
covertimpala <77851547+covertimpala@users.noreply.github.com>
2023-05-20 18:05:39 +0200

    Update strt.bat

--- strt.bat
+++ strt.bat
@@ -1,9 +1,7 @@
 @echo off
-timeout /t 5 /nobreak
 FOR /F "tokens=*" %%g IN ('python --version') do (SET string=%%g)
 set "versionn=Python 3"
 echo %string%
-timeout /t 5 /nobreak
 if %string:~0,1%==P (echo yes) else (python3 & timeout /t 5 /nobreak & start pressenter.vbs)
 until [[ $string != *"Python 3"* ]]
 start payload.pyw
