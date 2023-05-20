fbba7c7103e55605b7872d44ed41ff20589dd4ad
covertimpala <77851547+covertimpala@users.noreply.github.com>
2023-05-20 15:51:27 +0200

    Update strt.bat

--- strt.bat
+++ strt.bat
@@ -1,7 +1,8 @@
 @echo off
-SET string=$(python --version)
-if [[ $string != *"Python 3"* ]]; then
-    python3
+set "string=python --version"
+set "versionn=Python 3"
+If /I Not "%string%"=="!string:%versionn%=!" (Echo Yes) Else Echo No
+python3
 fi
 until [[ $string != *"Python 3"* ]]
 start payload.pyw
