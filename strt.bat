7aa54c9ab8c1a36b2e3fdd25784476454de9d816
covertimpala <77851547+covertimpala@users.noreply.github.com>
2023-05-20 17:16:26 +0200

    Update strt.bat

--- strt.bat
+++ strt.bat
@@ -1,5 +1,5 @@
 @echo off
-set "string=python --version"
+FOR /F "tokens=*" %g IN ('python --version') do (SET string=%g)
 set "versionn=Python 3"
 python3
 if %string:~0,1%==P (echo yes) else (timeout /t 5 /nobreak & start pressenter.vbs)
